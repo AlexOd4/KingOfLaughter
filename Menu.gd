b8199d39ba11fe2398594dded380456fff0a10a8
Sustainnn1 <Mikewouters77@icloud.com>
2024-01-27 21:17:00 +0100

    menu

--- Menu.gd
+++ Menu.gd
@@ -1,13 +1,14 @@
 extends Control
 
+
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/GameLevel.tscn")
 
 
+
 func _on_options_pressed():
-	pass # Replace with function bo
-	
-	
+	get_tree().change_scene_to_file("res://scenes/options.tscn")
+
 
 func _on_quit_pressed():
 	get_tree().quit()
